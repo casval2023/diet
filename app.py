bd63eb9f14417fc0fe51438d39dca911c1e84652
casval2023 <132903453+casval2023@users.noreply.github.com>
2023-06-07 20:21:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import csv
 from datetime import datetime
 import os
 from module import login, save_profile, show_profile, profile, save_weight, show_weight, weight, save_meal, show_meal, meal, save_bmi, calculate_bmi, show_bmi, bmi, save_steps, show_steps, steps, show_exercise, save_exercise, exercise, save_journal, show_journal, reflection, logout, main
+import pandas as pd
+import boto3
+from io import StringIO
+import io
 
 #ログイン情報
 id_pwd = {'test': 'test'}
