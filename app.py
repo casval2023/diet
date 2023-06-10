56fd9cfe7426fcf5051f47303090805753166372
casval2023 <132903453+casval2023@users.noreply.github.com>
2023-06-10 18:36:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,53 @@
 import streamlit as st
-import csv
-from datetime import datetime
-import os
-from module import login, save_profile, show_profile, profile, save_weight, show_weight, weight, save_meal, show_meal, meal, save_bmi, calculate_bmi, show_bmi, bmi, save_steps, show_steps, steps, show_exercise, save_exercise, exercise, save_journal, show_journal, reflection, logout, main
 import pandas as pd
 import boto3
-from io import StringIO
-import io
+import datetime
 
-#ログイン情報
-id_pwd = {'test': 'test'}
+# AWSのS3の設定
+s3 = boto3.client('s3', 
+                  region_name='your_region_name',
+                  aws_access_key_id='your_access_key',
+                  aws_secret_access_key='your_secret_key')
 
-if __name__ == '__main__':
-    main()
+bucket_name = 'your_bucket_name'
+
+def write_to_s3(df, filename):
+    csv_buffer = df.to_csv(index=False)
+    s3_resource = boto3.resource('s3')
+    s3_resource.Object(bucket_name, filename).put(Body=csv_buffer)
+
+# StreamlitのUIの設定
+st.title('Health & Fitness Tracker')
+
+# ユーザー情報の入力
+st.subheader('User Information')
+height = st.number_input('Height (cm)', min_value=100.0, max_value=250.0)
+weight = st.number_input('Weight (kg)', min_value=30.0, max_value=200.0)
+body_fat = st.number_input('Body Fat (%)', min_value=5.0, max_value=50.0)
+age = st.number_input('Age', min_value=10, max_value=100)
+gender = st.selectbox('Gender', ['Male', 'Female'])
+
+# タスクの設定
+st.subheader('Today\'s Tasks')
+tasks = ['Consume less than 1500 kcal',
+         'Walk 10,000 steps',
+         'Watch 1 chapter of video lecture',
+         'Challenge 1 chapter of review questions',
+         'Workout']
+task_status = [st.checkbox(task) for task in tasks]
+
+# 入力データの保存
+if st.button('Submit'):
+    data = {'Height': height, 'Weight': weight, 'Body Fat': body_fat,
+            'Age': age, 'Gender': gender, 
+            'Tasks': tasks, 'Task Status': task_status,
+            'Date': datetime.date.today()}
+    df = pd.DataFrame(data, indexすみません、前回のメッセージが途中で切れてしまいました。以下が続きのコードです。
+
+```python
+    df = pd.DataFrame(data, index=[0])
+    
+    # データをS3に保存
+    write_to_s3(df, f'data_{datetime.date.today()}.csv')
+
+    st.success('Data submitted successfully.')
